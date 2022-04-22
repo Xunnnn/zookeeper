76654386847b130814334e426ef6ec589123a50f
wangxun <whutwangxun@qq.com>
2022-04-22 16:40:48 +0800

    test

--- src/main/java/com/wang/zkcase2/DistributedLockTest.java
+++ src/main/java/com/wang/zkcase2/DistributedLockTest.java
@@ -8,19 +8,16 @@ public class DistributedLockTest {
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         final DistributedLock lock1 = new DistributedLock();
         final DistributedLock lock2 = new DistributedLock();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    lock1.zkLock();
-                    System.out.println("线程1获取锁");
-                    Thread.sleep(5 * 1000);
+        new Thread(() -> {
+            try {
+                lock1.zkLock();
+                System.out.println("线程1获取锁");
+                Thread.sleep(5 * 1000);
 
-                    lock1.zkUnlock();
-                    System.out.println("线程1释放锁");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                lock1.zkUnlock();
+                System.out.println("线程1释放锁");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }).start();
 
